982255c1f8372d71e66aba3d0190c3ed2fad6527
Daniellewoessner <dlegare7792@gmail.com>
2025-02-20 16:52:03 -0500

    changes for render functionality

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,17 +5,36 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+let sequelize: Sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  // Render production configuration
+  sequelize = new Sequelize({
+    host: 'dpg-curn51ij1k6c73aurff0-a',
+    database: 'kanban_db_rlql',
+    username: 'kanban_db_rlql_user',
+    password: '9L8ygAiyZrHSJLDAlDW3AqjQUFBVnwRF',
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      decimalNumbers: true
     },
-    decimalNumbers: true
-  },
-  logging: false
-});
+    logging: false
+  });
+} else {
+  // Local development configuration
+  sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+}
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
